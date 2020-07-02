3a25bc98499e44e383f081df23bb46efcdd4aad9
minhthao <minhthao5648@gmail.com>
2020-07-02 19:14:37 +0700

    build app

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 const userRouter = require("./routers/users.router");
 const financeRouter = require("./routers/finance.router");
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Cors
 
@@ -17,10 +17,12 @@ app.use(cors());
 //Connect monogoBD
 const uri =
   "mongodb+srv://minhthao56:minhthao56@cluster0-dfzmq.gcp.mongodb.net/financeApp?retryWrites=true&w=majority";
-mongoose.connect(uri, {
+const url2 =
+  "mongodb://minhthao56:minhthao56@ds125060.mlab.com:25060/heroku_lc77pdj0";
+mongoose.connect(uri || url2, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 const connection = mongoose.connection;
 connection.once("open", () => {
