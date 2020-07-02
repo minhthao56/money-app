7cf800260d1b8b53740f25c023ffe4b7423ae405
minhthao <minhthao5648@gmail.com>
2020-07-02 19:25:12 +0700

    add build

---
+++ client/build/precache-manifest.7778b33d4c8aa99cf3f849f375777578.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1ae35321ac0c1b75d618f887c4bdcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e05c68c86bba65428bdb",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "0a12d5035706326c2e35",
+    "url": "/static/css/main.275e09a0.chunk.css"
+  },
+  {
+    "revision": "e05c68c86bba65428bdb",
+    "url": "/static/js/2.9c690c4f.chunk.js"
+  },
+  {
+    "revision": "7d1ecf83a7d04a3236a7607ad2584ef6",
+    "url": "/static/js/2.9c690c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a12d5035706326c2e35",
+    "url": "/static/js/main.3c145daf.chunk.js"
+  },
+  {
+    "revision": "9a5b565b6db84a9e4065",
+    "url": "/static/js/runtime-main.b4caa378.js"
+  },
+  {
+    "revision": "47135c4c0e9bc825dffa3cf7905d3561",
+    "url": "/static/media/Login.47135c4c.svg"
+  },
+  {
+    "revision": "fdcbee53cf2ca5a2b1317769e83bf26b",
+    "url": "/static/media/Wellcome.fdcbee53.svg"
+  },
+  {
+    "revision": "ec628499722afe93023dbf6767783b57",
+    "url": "/static/media/finance.ec628499.png"
+  },
+  {
+    "revision": "cce4ca88dd6387f8ddd0ad792b0e912f",
+    "url": "/static/media/selfie.cce4ca88.svg"
+  },
+  {
+    "revision": "ae5e82b3a7d9e4c1fdcf5fe7a8ee5188",
+    "url": "/static/media/time.ae5e82b3.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
